c6776dc08fc35fccc102700d89bfca7424116c5e
Javier Aguilar <coyote176@msn.com>
2020-08-22 20:26:12 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,36 +1,29 @@
-class Artist 
-  
-  attr_accessor :name  
-  
-    def initialize(name)
-      @name= name
-    end
-    
-    def add_song(song)
-      song.artist= self 
-    end
-    
-    def self.all
-      @@all
-    end 
-    
-    def add_song_by_name(name)
-      song= Song.new(name)
-      add_song(song)
-    end
-    
-    def songs
-    Song.all.select do |song|
-      song.artist == self
-      end
-    end
-    
-    def self.song_count
-      Song.all.count
-    end
+class Artist
 
-  end 
-  
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def add_song_by_name(title)
+    song = Song.new(title)
+    add_song(song)
+  end
+
+  def add_song(song)
+      song.artist = self
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def self.song_count
+    Song.all.count
+  end
+
+end
    
 
  
