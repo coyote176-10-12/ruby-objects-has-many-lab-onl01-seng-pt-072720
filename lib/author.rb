e5d96370f6d427f4a92eba4a706ad8634db89507
Javier Aguilar <coyote176@msn.com>
2020-08-22 20:27:32 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,28 +1,26 @@
 class Author
-  attr_accessor :name, :posts
+
+  attr_accessor :name
 
   def initialize(name)
     @name = name
-    @posts = []
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    add_post(post)
   end
 
   def add_post(post)
-   @posts << post
-   post.author = self
-  end
-
-  def add_post_by_title(post_title)
-   new_post = Post.new(post_title)
-   new_post.author = self
+      post.author = self
   end
 
   def posts
-   Post.all.select {|post| post.author = self}
+    Post.all.select {|post| post.author == self}
   end
-
 
   def self.post_count
-   post_count =+ 3
+    Post.all.count
   end
-
+  
 end
