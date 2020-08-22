1b2731b22f2c2de7d0ea997dcd8e76feccb7809a
Javier Aguilar <coyote176@msn.com>
2020-08-22 19:58:26 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -13,31 +13,4 @@ class Author
     
     
   end 
-  def initialize(name)
-      @name= name
-    end
-    
-    def add_song(song)
-      song.artist= self 
-    end
-    
-    def self.all
-      @@all
-    end 
-    
-    def add_song_by_name(name)
-      song= Song.new(name)
-      add_song(song)
-    end
-    
-    def songs
-    Song.all.select do |song|
-      song.artist == self
-      end
-    end
-    
-    def self.song_count
-      Song.all.count
-    end
-
-  end 
+  
